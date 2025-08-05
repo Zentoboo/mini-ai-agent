7f35e3e4c0abb6928a9c03697df7e5d27a14e6ea
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-08-05 17:22:43 +0800

    FEAT: API connection with successful prompt and response

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
     print("Hello from mini-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        print("Missing GEMINI_API_KEY in environment.")
+        return
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
