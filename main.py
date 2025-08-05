f2f62c747b71c7954efa60141ca78781f87bf8cb
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-08-05 17:26:49 +0800

    FEAT: added prompting on command line

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import os
+import os 
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
     print("Hello from mini-ai-agent!")
+    
+    if len(sys.argv) < 2:
+        print("Usage: python main.py \"Your prompt here...\"")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         print("Missing GEMINI_API_KEY in environment.")
-        return
+        sys.exit(1)
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_prompt
     )
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
