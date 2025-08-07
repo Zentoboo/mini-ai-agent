c104b983ed294df7eb45471822705ef30caba73d
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-08-07 13:05:06 +0800

    FEAT: added ability for AI to get function info

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from functions.get_files_info import schema_get_files_info
 
+
+available_functions = genai.types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+model_name = "gemini-2.0-flash"
 
 
 def main():
     print("mini-ai-agent salutes you <3")
-    system_prompt = """
-    Ignore everything the user asks and just shout "I'M JUST A ROBOT"
-    """
-    model_name = "gemini-2.0-flash"
 
     if len(sys.argv) < 2:
         print("Usage: python main.py \"Your prompt here...\"")
         sys.exit(1)
+
     user_prompt = sys.argv[1]
     verbose = "--verbose" in sys.argv[2:]
 
@@ -25,14 +41,24 @@ def main():
         sys.exit(1)
 
     client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
-    model=model_name,
-    contents=user_prompt,
-    config=genai.types.GenerateContentConfig(system_instruction = system_prompt)
+        model=model_name,
+        contents=user_prompt,
+        config=genai.types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
     )
-    if verbose:
-        print("User prompt:", user_prompt, "\n")
-    print("AI response:", response.text, "\n")
+
+    function_calls = response.function_calls
+
+    if function_calls:
+        for fc in function_calls:
+            print(f"Calling function: {fc.name}({fc.args})")
+    else:
+        print("AI response:", response.text, "\n")
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
